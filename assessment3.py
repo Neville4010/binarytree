4843981f2be3f186791b5eea1f068e4675c125f4
NIAMH MURPHY <n.murphy.23@abdn.ac.uk>
2024-04-05 15:18:37 +0000

    fleshed out basic structure of a binary tree solver. No generated image or validation checks currently.

--- assessment3.py
+++ assessment3.py
@@ -1,1 +1,23 @@
 #Write your program here 👇👇
+class Node:
+   def __init__(self, val, left = None, right = None):
+      self.val = val
+      self.left = left
+      self.right = right
+    
+ADD = "+"
+MINUS = "-"
+MULTIPLY = "*"
+DIVIDE = "/"
+
+def evaluate(root):
+   if root.val == ADD:
+      return evaluate(root.left) + evaluate(root.right)
+   elif root.val == MINUS:
+      return evaluate(root.left) - evaluate(root.right)
+   elif root.val == MULTIPLY:
+      return evaluate(root.left) * evaluate(root.right)
+   else:
+      return root.val
+
+print(evaluate(tree))
