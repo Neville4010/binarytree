23648e82d72c61c0069c63cf8f58c7f8610948d9
NIAMH MURPHY <n.murphy.23@abdn.ac.uk>
2024-04-24 11:05:08 +0000

    creation of expression tree in postfix order. Next step to generate preorder, postorder, and inorder display.

--- assessment3.py
+++ assessment3.py
@@ -3,7 +3,14 @@
 mathEquation = input("please input your maths equation in the format of (X?Y) and this will convert it to a binary tree :): ")
 mathEquation = mathEquation.replace(' ', '')
 postFix = ' '
-nodeValues = []
+nodeValues = ""
+
+class Node:
+  def __init__(self, value = None, left = None, right = None, next = None):
+    self.data = value
+    self.left = left
+    self.right = right
+    self.next = next
 
 def prec(equation):
   if equation == '^':
@@ -24,7 +31,7 @@ def infixOrPostfix(equation):
   result = []
   stack = []
   global nodeValues
-  nodeValues = [result]
+  nodeValues = ' '.join(result)
 
   for i in range(len(mathEquation)):
     equation = mathEquation[i]
@@ -46,7 +53,55 @@ def infixOrPostfix(equation):
     result.append(stack.pop())
   
   print(''.join(result))
-  return nodeValues
- 
-infixOrPostfix(mathEquation)
-print(nodeValues)
+
+  for i in result:
+    nodeValues += i +""
+
+class Stack:
+  def __init__(self):
+    self.head = None
+  
+  def push(self, node):
+    if not self.head:
+      self.head = node
+    else:
+      node.next = self.head
+      self.head = node
+  
+  def pop(self):
+    if self.head:
+      popped = self.head
+      self.head = self.head.next
+      return popped
+    else:
+      raise Exception("Stack is empty")
+
+class binaryExpressionTree:
+  def inorder(self, x):
+    if not x:
+      return
+    self.inorder(x.left)
+    print(x.value, end = " ")
+    self.inorder(x.right)
+
+def main():
+  stack = Stack()
+  tree = binaryExpressionTree()
+
+  for c in nodeValues:
+    if c in "+-*/^":
+      z = Node(c)
+      x = stack.pop()
+      y = stack.pop()
+      z.left = y
+      z.right = x
+      stack.push(z)
+    else:
+      stack.push(Node(c))
+  print("The Inorder Traversal of Expression Tree: ", end = " ")
+  infixOrPostfix(mathEquation)
+  print(nodeValues)
+
+
+if __name__ == "__main__":
+  main()
