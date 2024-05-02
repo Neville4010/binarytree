49ff779145127c4aad39eac849475bed2247aebb
NIAMH MURPHY <n.murphy.23@abdn.ac.uk>
2024-05-02 21:45:08 +0000

    writing of readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+Expression Binary Tree
+Second Assessment for CS1527 Object-Orientated Programming
+
+To run the program, the user will be prompted to input their mathematical equation that should be in the format (X?Y) where the X and Y are operands (digits between 0 and 9) and ? is the operator +,-,/,*.
+Validation checks for user input are:
+1. mismatched brackets
+
+2. wrong number of operands
+
+3. wrong number of operators
--- assessment3.py
+++ assessment3.py
@@ -20,22 +20,31 @@ def validExpression(expression):
   operators = 0
   openBrackets = expression.count('(')
   closedBrackets = expression.count(')')
+  bracketSet = openBrackets / 2
+  totalBrackets = openBrackets - closedBrackets
   errorMessage = "Not a valid expression,"
+  error = False
   for thing in expression:
     if thing == "+" or thing == "-" or thing == "/" or thing == "*":
       operators += 1
     elif thing.isdigit() :
       operands += 1
-  if openBrackets != closedBrackets:
-    errorMessage = errorMessage + " brackets are mismatched,"
-  if operators > (operands/2) or operators < (operands/2):
-    errorMessage = errorMessage + " wrong amount of operands,"
+  for i in range(1, len(expression)):
+    if expression[i-1] in "+-/*" and expression[i] in "+-/*":
+      errorMessage = errorMessage + " 2. wrong amount of operands"
+      error = True
+  if totalBrackets != 0:
+    errorMessage = errorMessage + " 1. brackets are mismatched"
+    error = True
   if operands % 2 != 0:
-    errorMessage = errorMessage + " missing operator."
-  print(errorMessage)
-  quit()
+    errorMessage = errorMessage + " 3. missing operator."
+    error = True
+  if error:
+    print(errorMessage)
+    quit()
 
-def print2DUtil(x, space):
+#this code's inpsiration was taken from https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/ with alterations to variable and function names.
+def printExpressionTree(x, space):
  
     # Base case
     if (x == None):
@@ -45,7 +54,7 @@ def print2DUtil(x, space):
     space += COUNT[0]
  
     # Process right child first
-    print2DUtil(x.right, space)
+    printExpressionTree(x.right, space)
  
     # Print current node after space
     # count
@@ -55,13 +64,13 @@ def print2DUtil(x, space):
     print(x.value)
  
     # Process left child
-    print2DUtil(x.left, space)
- 
-def print2D(x):
- 
+    printExpressionTree(x.left, space)
+
+#this code's inpsiration was taken from https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/ with alterations to variable and function names.
+def getPrintExpressionTree(x):
     # space=[0]
     # Pass initial space count as 0
-    print2DUtil(x, 0)
+    printExpressionTree(x, 0)
 
 #function to determine the precedence of operators
 def operatorOrder(equation):
@@ -102,7 +111,6 @@ def infixOrPostfix(equation):
   while postfixStack:
    result.append(postfixStack.pop())
 
-  print(''.join(result))
   nodeValues = ''.join(result)
 #creation of postfix order string
 
@@ -110,6 +118,7 @@ def infixOrPostfix(equation):
 
  
 class Stack:
+#This code's inspiration was drawn from https://www.geeksforgeeks.org/expression-tree/ with alterations to variable names and additional code.
     def __init__(self):
       self.head = None
  
@@ -131,7 +140,8 @@ class Stack:
     def top(self):
         return self.head
  
-class ExpressionTree:
+class ExpressionTreeTraversal:
+#This class's code inspiration was drawn from https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/ with alterations to variable names.
   def inorder(self, x):
 #Base Case
     if not x:
@@ -178,34 +188,33 @@ class ExpressionTree:
       if node.right is not None:
         treeQueue.append(node.right)
     return breadthFirst
-       
+
+#This code's inspiration was drawn from https://www.geeksforgeeks.org/expression-tree/ with alterations to variable names and additional code to tailor to assessment requirements.
 def main():
   validExpression(mathEquation)
   postfixEquation = infixOrPostfix(mathEquation)
   stack = Stack()
-  tree = ExpressionTree()
+  treeTraversal = ExpressionTreeTraversal()
   for character in postfixEquation:
     if character in "+-*/^":
       root = Node(character)
-      print(root.value)
       x = stack.pop()
-      print(x.value)
       y = stack.pop()
       root.left = y
       root.right = x
       stack.push(root)
     else:
       stack.push(Node(character))
-  print("The Inorder Traversal of Expression Tree: \n", end=" ")
-  tree.inorder(stack.top())
-  print("The Preorder Traversal of Expression Tree: \n", end=" ")
-  tree.preorder(stack.top())
-  print("The Postorder Traversal of Expression Tree: \n", end=" ")
-  tree.postorder(stack.top())
-  print("The Breadth First Traversal of Expression Tree: \n", end=" ")
-  tree.breadthFirstTraversal(stack.top())
+  print("The Inorder Traversal of Expression Tree: ", end=" ")
+  treeTraversal.inorder(stack.top())
+  print("The Preorder Traversal of Expression Tree: ", end=" ")
+  treeTraversal.preorder(stack.top())
+  print("The Postorder Traversal of Expression Tree: ", end=" ")
+  treeTraversal.postorder(stack.top())
+  print("The Breadth First Traversal of Expression Tree: ", end=" ")
+  treeTraversal.breadthFirstTraversal(stack.top())
   print("A visual representation of the tree :) \n", end=" ")
-  print2D(stack.top())
+  getPrintExpressionTree(stack.top())
     
  
 if __name__ == "__main__":
